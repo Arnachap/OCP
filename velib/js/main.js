b08d3d330aa20c6f3efa83516fcb65a45d96cba0
Arnachap <arnaud.chapeleur@gmail.com>
2017-12-04 11:15:46 +0100

    work in progress

--- velib/js/main.js
+++ velib/js/main.js
@@ -1,56 +1,55 @@
 function newSelected(element) {
 
-  if (element === "next") {
-    var selected = $(".selected").next();
-  } else if (element == "previous") {
-    var selected = $(".selected").prev();
-  } else {
-    var selected = element;
-  }
+	if (element === "next") {
+		var selected = $(".selected").next();
+	} else if (element == "previous") {
+		var selected = $(".selected").prev();
+	} else {
+		var selected = element;
+	}
 
-  var next = $(selected).next();
-  var previous = $(selected).prev();
-  var previousSecond = $(previous).prev();
-  var nextSecond = $(next).next();
+	var next = $(selected).next();
+	var previous = $(selected).prev();
 
-  $(selected).removeClass().addClass("selected");
+	var nextSecond = $(next).next();
+	var previousSecond = $(previous).prev();
 
+	$(selected).removeClass().addClass("selected");
+
+	$(next).removeClass().addClass("next");
   $(previous).removeClass().addClass("previous");
-  $(next).removeClass().addClass("next");
 
-  $(nextSecond).removeClass().addClass("nextSecond");
-  $(previousSecond).removeClass().addClass("previousSecond");
+	$(nextSecond).removeClass().addClass("nextSecond");
+	$(previousSecond).removeClass().addClass("previousSecond");
 
-  $(nextSecond).nextAll().removeClass().addClass('hideNext');
-  $(previousSecond).prevAll().removeClass().addClass('hidePrevious');
-
+	$(nextSecond).nextAll().removeClass().addClass('hideNext');
+	$(previousSecond).prevAll().removeClass().addClass('hidePrevious');
+  
 }
 
-// Eventos teclado
 $(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-        newSelected('previous');
-        break;
+	switch(e.which) {
+		case 37:
+		newSelected('previous');
+		break;
 
-        case 39: // right
-        newSelected('next');
-        break;
+		case 39:
+		newSelected('next');
+		break;
 
-        default: return;
-    }
-    e.previousentDefault();
+		default: return;
+	}
+	e.preventDefault();
 });
 
 $('#slideshow div').click(function() {
-  newSelected($(this));
+	newSelected($(this));
 });
 
 $('#previous').click(function() {
-  newSelected('previous');
+	newSelected('previous');
 });
 
 $('#next').click(function() {
-  newSelected('next');
+	newSelected('next');
 });
-
